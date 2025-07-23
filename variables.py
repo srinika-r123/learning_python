de4d218f0bb16d5d972ed052421a9ded20ba7616
srinika-r123 <srinikad123@gmail.com>
2025-07-23 18:11:19 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,24 @@
+#A variable is created when you assign a value to it. 
+name = "srinika"
+age = 16
+x = 15
+y = 75
+print(name)
+print(age)
+print(x*y)
+print(x/y)
+print(x+y)
+print(x-y)
+#I have printed x and y using basic arithmetic operations
+
+#You can specify the data type of the variable through casting.
+a = str(26)
+#the str function just keeps the characters side by side
+b = int(26)
+#the int function is the integer value
+c = float(26)
+#the float function basically adds a decimal point to an integer
+print(a)
+print(b)
+print(c)
+#last updated July 23rd, 2025
