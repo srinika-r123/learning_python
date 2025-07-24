a6289527ea32a3b1f3c076d1c75a1cafb05364d1
srinika-r123 <srinikad123@gmail.com>
2025-07-24 17:38:32 +0530

    Add files via upload

---
+++ variables2.py
@@ -0,0 +1,16 @@
+#Multiple variables can be assigned in one line 
+x, y, z = "Red", "Orange", "Yellow"
+print(x, y, z)
+#I have printed the variables in one line.
+
+#I can assign one value to multiple variables
+d = e = f = "Blue"
+print(d, e, f)
+
+#You can identify the data type of the variable by using the function "type".
+a = "Srinika"
+b = 26
+c = 98.76
+print(type(a))
+print(type(b))
+print(type(c))
