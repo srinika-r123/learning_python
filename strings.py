233ee142b221ed47a0da021332852954370fc34d
srinika-r123 <srinikad123@gmail.com>
2025-07-26 17:53:23 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,11 @@
+x = """This is a multi-line
+string. You can use three
+double quotes."""
+print(x)
+
+y = '''This is also a multi-line
+string. You can also use three
+single quotes.'''
+print(y)
+
+#Last updated on July 26th, 2025
