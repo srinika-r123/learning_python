0e9356801f0d17d189b40d8d832a554b37265452
srinika-r123 <srinikad123@gmail.com>
2025-07-26 17:39:59 +0530

    Add files via upload

---
+++ variables3.py
@@ -0,0 +1,12 @@
+x = "interesting"
+def myfunc():
+  x = "cool"
+  print("Cyberlaw is " + x)
+
+myfunc()
+#myfunc() outputs what I defined earlier
+
+print("Cyberlaw is " + x)
+#This will output x as interesting because it is predefined and not a part of myfunc()
+
+#Last updated on July 26th, 2025
