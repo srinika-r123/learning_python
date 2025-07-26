048e09b8249919a084165cc32adfa7cd894fc775
srinika-r123 <srinikad123@gmail.com>
2025-07-26 17:53:04 +0530

    Add files via upload

---
+++ random.py
@@ -0,0 +1,7 @@
+import random
+#Python does not have a random() function, but it has a module which is used to generate random numbers.
+
+print(random.randrange(1, 10))
+#A random number ranging from 1 to 10 is displayed.
+
+#Last updated on July 26th, 2025
